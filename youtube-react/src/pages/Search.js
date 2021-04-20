c997e2e310226f2278094b5be5a0e646aaa2177b
Fujiwara <r9.20important@icloud.com>
2021-04-21 06:48:11 +0900

    headerの追加、headerのスタイリング

---
+++ youtube-react/src/pages/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+
+function Search() {
+    return (
+        <Layout>
+            search page
+        </Layout>
+    )
+}
+
+export default Search
